fa8aa03817184e0deae1f9a1fc1feee9b4ecfe97
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-04-25 04:40:03 -0300

    Substituindo aspas por apóstrofos para manter um padrão. [UPDATE]

--- src/components/player/player.tsx
+++ src/components/player/player.tsx
@@ -2,33 +2,33 @@ import styles from './player.module.scss';
 
 export default function Player() {
   return (
-    <div className={styles['player-container']}>
+    <div className={styles["player-container"]}>
       <header>
         <img src="/playing.svg" alt="Tocando agora"/>
         <strong>Tocando agora</strong>
       </header>
 
-      <div className={styles['empty-player']}>
+      <div className={styles["empty-player"]}>
         <strong>Selecione um podcast para ouvir</strong>
       </div>
 
-      <footer className={styles['empty']}>
-        <div className={styles['progress']}>
+      <footer className={styles["empty"]}>
+        <div className={styles["progress"]}>
           <span>00:00</span>
-          <div className={styles['slider']}>
-            <div className={styles['empty-slider']} />
+          <div className={styles["slider"]}>
+            <div className={styles["empty-slider"]} />
           </div>
           <span>00:00</span>
         </div>
 
-        <div className={styles['buttons']}>
+        <div className={styles["buttons"]}>
           <button type="button">
             <img src="/shuffle.svg" alt="Embaralhar"/>
           </button>
           <button type="button">
             <img src="/play-previous.svg" alt="Tocar anterior"/>
           </button>
-          <button type="button" className={styles['play-button']}>
+          <button type="button" className={styles["play-button"]}>
             <img src="/play.svg" alt="Tocar"/>
           </button>
           <button type="button">
