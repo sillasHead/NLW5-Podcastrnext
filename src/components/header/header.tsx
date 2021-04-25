0ac48cd845d38e7a9022958144c4c72b949c2f53
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-04-24 23:39:23 -0300

    Adicionando linha em branco ao final dos arquivos. [WORK]

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,12 +7,12 @@ export default function Header() {
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
     locale: ptBR, //§ objeto de configuracoes
   });
-  
+
   return (
     <header className={styles['header-container']}>
       <img src="/logo.svg" alt="Podcastr"/>
 
-      <p>O melhor para você ouvir, sempre</p>   
+      <p>O melhor para você ouvir, sempre</p>
 
       <span>{currentDate}</span>
     </header>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,4 +19,4 @@ export default class MyDocument extends Document {
       </Html>
     );
   }
-} 
+}
