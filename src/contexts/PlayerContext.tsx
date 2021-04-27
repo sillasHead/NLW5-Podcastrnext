511273503d058f7d77c760c1d01afde8cd5554fc
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-04-26 22:43:24 -0300

    Unificando funções do contexto do player em um único arquivo. [UPDATE]
    Passando as funções que antes estavam diretamente no arquivo que exibe as páginas para o arquivo de seu contexto para um melhor entendimento e uma fácil manutenção.

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,124 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+
+type Episode = {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
+};
+
+type PlayerContextData = {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+  isPlaying: boolean;
+  isLooping: boolean;
+  isShuffling: boolean;
+  play: (episode: Episode) => void;
+  playList: (list: Episode[], index: number) => void;
+  togglePlay: () => void;
+  toggleLoop: () => void;
+  toggleShuffle: () => void;
+  setPlayingState: (state: boolean) => void;
+  clearPlayerState: () => void;
+  playNext: () => void;
+  playPrevious: () => void;
+  hasNext: boolean;
+  hasPrevious: boolean;
+};
+
+export const PlayerContext = createContext({} as PlayerContextData);
+//§ passo por parametro o formato dos dados que ele ira receber
+
+type PlayerContextProviderProps = {
+  children: ReactNode; //§ReactNode qualquer elemento que o react aceita dentro do .jsx
+}
+
+export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isLooping, setIsLooping] = useState(false);
+  const [isShuffling, setIsShuffling] = useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function playList(list: Episode[], index: number) {
+    setEpisodeList(list);
+    setCurrentEpisodeIndex(index);
+    setIsPlaying(true); // ao iniciar outro podcast, ja deve ser iniciado, mesmo se o anterior estiver pausado
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function toggleLoop() {
+    setIsLooping(!isLooping);
+  }
+
+  function toggleShuffle() {
+    setIsShuffling(!isShuffling);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  function clearPlayerState() {
+    setEpisodeList([]);
+    setCurrentEpisodeIndex(0);
+  }
+
+  const hasNext = isShuffling || (currentEpisodeIndex + 1) < episodeList.length;
+  const hasPrevious = currentEpisodeIndex > 0;
+
+  function playNext() {
+    if (isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length);
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex);
+    } else if (hasNext) {
+      setCurrentEpisodeIndex(currentEpisodeIndex + 1);
+    }
+  }
+
+  function playPrevious() {
+    if (hasPrevious) {
+      setCurrentEpisodeIndex(currentEpisodeIndex - 1);
+    }
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        isPlaying,
+        isLooping,
+        isShuffling,
+        play,
+        playList,
+        togglePlay,
+        toggleLoop,
+        toggleShuffle,
+        setPlayingState,
+        clearPlayerState,
+        playNext,
+        playPrevious,
+        hasNext,
+        hasPrevious,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+    //§ todos os componentes dentro desse contexto tem acesso aos dados dentro dele
+  );
+}
+
+export const usePlayer = () => {
+  return useContext(PlayerContext);
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,13 @@
-import { useState } from 'react';
 import Header from '../components/header/header';
 import Player from '../components/player/player';
-import { PlayerContext } from '../contexts/PlayerContext';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 import styles from '../styles/app.module.scss';
 import '../styles/global.scss';
 
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
-
-  return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
-    {/* § todos os componentes dentro desse contexto tem acesso aos dados dentro dele */}
+  return(
+    <PlayerContextProvider>
       <div className={styles['wrapper']}>
         <main>
           <Header />
@@ -35,7 +15,7 @@ function MyApp({ Component, pageProps }) {
         </main>
         <Player />
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   );
 }
 
